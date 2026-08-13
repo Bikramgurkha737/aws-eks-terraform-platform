13b143e0007351cc7174a4755fad4dc1d8245311
Bikram Thapa <bikram.gurkha737@gmail.com>
2026-08-12 19:08:51 -0500

    Harden EKS network access and subnet configuration

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_1_cidr
   availability_zone       = data.aws_availability_zones.available.names[0]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name                     = "${var.cluster_name}-public-1"
@@ -37,7 +37,7 @@ resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_2_cidr
   availability_zone       = data.aws_availability_zones.available.names[1]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name                     = "${var.cluster_name}-public-2"
