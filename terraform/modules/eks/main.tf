e4322e3da4bc8785f09417b6d7f0f185af14fbc0
Bikram Thapa <bikram.gurkha737@gmail.com>
2026-08-12 19:13:06 -0500

    Enable KMS encryption for EKS secrets

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,6 +1,29 @@
+resource "aws_kms_key" "eks" {
+  description             = "KMS key for EKS Kubernetes secrets encryption"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  tags = {
+    Name = "${var.cluster_name}-eks-secrets"
+  }
+}
+
+resource "aws_kms_alias" "eks" {
+  name          = "alias/${var.cluster_name}-eks-secrets"
+  target_key_id = aws_kms_key.eks.key_id
+}
+
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   role_arn = var.cluster_role_arn
+
+  encryption_config {
+    provider {
+      key_arn = aws_kms_key.eks.arn
+    }
+
+    resources = ["secrets"]
+  }
 
   vpc_config {
     subnet_ids = var.subnet_ids
